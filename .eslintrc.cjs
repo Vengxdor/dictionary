dbc273b3d9bcedec45e8e7078a02db0a6744cc02
steven <Fernandezsteven185@gmail.com>
2024-03-23 19:54:32 -0500

    Add basic project structure with Vite, TypeScript, Tailwind CSS, and ESLint configured.

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
