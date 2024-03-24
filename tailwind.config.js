59021c88069887a630b2e9507d5a825a2659fe76
steven <Fernandezsteven185@gmail.com>
2024-03-23 21:11:55 -0500

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'selector',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary-500': '#891cdb', // purple
+        'primary-400': '#aa45fa',
+        'primary-200': '#e9d0fa',
+        'dark-primary-500': '#4d2e6d',
+        'dark-primary-400': '#a047de',
+        'dark-primary-200': '#2b143c',
+        'dark-background': '#050505'
+      }
+    }
   },
   plugins: []
 }
