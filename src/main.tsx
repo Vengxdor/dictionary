36a328e817f49d0d32396f1a583784fbfe92e1dd
steven <Fernandezsteven185@gmail.com>
2024-03-24 17:38:19 -0500

    fix: Get rid of unnecessary code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { WordProvider } from './context/wordContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <WordProvider>
     <App />
-  </React.StrictMode>
+  </WordProvider>
 )
--- src/sections/SearchWord.tsx
+++ src/sections/SearchWord.tsx
@@ -2,20 +2,21 @@ import { FormEvent, useState } from 'react'
 import { searchWord } from '../service/dictionarySearch'
 
 function SearchWord () {
-  const [word, setWord] = useState('')
+  const [newWord, setNewWord] = useState('')
   const handleSearch = async (e: FormEvent) => {
     e.preventDefault()
-    if (word.trim() === '') return
+    if (newWord.trim() === '') return
 
-    const data = await searchWord(word)
-    console.log(data)
-    setWord('')
+    const data = await searchWord(newWord)
+    // Store the word in localStorage.
+    localStorage.setItem('word', JSON.stringify(data))
+    setNewWord('')
   }
   return (
     <form onSubmit={handleSearch} className="flex bg-zinc-800 rounded-lg">
       <input
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
+        value={newWord}
+        onChange={(e) => setNewWord(e.target.value)}
         className="bg-transparent p-2 outline-none flex-grow" placeholder="Search a word..." type="text" />
       <button className="fa-solid fa-search  p-3 text-dark-primary-400" type="submit"></button>
     </form>
