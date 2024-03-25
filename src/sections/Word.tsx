eb2ba15a718653e43675d974e0d9619c20321925
steven <Fernandezsteven185@gmail.com>
2024-03-24 22:55:43 -0500

    Feat: Finish the word and phonetic header.

--- src/sections/Word.tsx
+++ src/sections/Word.tsx
@@ -1,9 +1,15 @@
-function Word ({ word }: {word: object}) {
-  console.log(word)
+import { DictionaryRes } from '../service/dictionarySearch'
+
+function Word ({ word }: {word: DictionaryRes}) {
   return (
-    <header>
-      <h2>Word</h2>
-      <p>phonetic</p>
+    <header className="flex justify-between items-center my-5">
+      <div>
+        <h2 className="text-3xl">{word.word}</h2>
+        <p className="text-dark-primary-400 dark:text-primary-400">{word.phonetic}</p>
+      </div>
+      <button className="bg-dark-primary-500/50 w-11 h-11 rounded-full cursor-pointer dark:bg-primary-200 flex justify-center items-center">
+        <i className="fa-solid fa-play text-dark-primary-400 dark:text-primary-400"></i>
+      </button>
     </header>
   )
 }
