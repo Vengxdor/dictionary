12542667be498a32b5f14df572dd8ef165311931
steven <Fernandezsteven185@gmail.com>
2024-03-25 03:20:46 -0500

    fix: Add icon to link.

--- src/sections/Source.tsx
+++ src/sections/Source.tsx
@@ -3,12 +3,15 @@ import { useWordContext } from '../hooks/useWordContext'
 function Source () {
   const { wordInfo } = useWordContext()
   return (
-    <footer className='mt-6 pt-6 border-t border-white/10 dark:border-zinc-200'>
+    <footer className='my-6 py-6 border-t border-white/10 dark:border-zinc-200'>
       {/* <hr className=''/> */}
       <h2 className='text-white/60  dark:text-zinc-400 text-lg'>Source</h2>
 
       {wordInfo.sourceUrls?.map((url, index) => (
-        <a className='underline' key={index} href={url}>{url}</a>
+        <a className='underline flex gap-2 items-center cursor-pointer' key={index} href={url}>
+          {url}
+          <i className="fa-solid fa-up-right-from-square opacity-50 fa-sm"></i>
+        </a>
       ))}
     </footer>
   )
